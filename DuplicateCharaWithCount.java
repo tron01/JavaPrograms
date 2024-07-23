432f5c1a32a97f1cbfcb7fc9c79caa6c1e3e53bf
tron01 <69385258+tron01@users.noreply.github.com>
2024-07-23 23:57:13 +0530

    Create DuplicateCharaWithCount.java

---
+++ DuplicateCharaWithCount.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+
+class DuplicateCharaWithCount{
+
+public static void main(String args[]){
+    
+    HashMap<Character,Integer> duplicateMap =new HashMap<>();
+    String str ="abcdaabbccddee";
+
+    char[] strArry =str.toCharArray();
+
+
+    for(char keys: strArry){
+
+        // if a in hasmap update its count else add a to hashmap nd its count=0 
+        if(duplicateMap.containsKey(keys)){
+            //updating the count
+            duplicateMap.put(keys,duplicateMap.get(keys)+1);
+        }else{
+            //adding to the hashmap
+            duplicateMap.put(keys,1);
+        }
+    }
+    System.out.println(duplicateMap);
+
+}
+
+
+
+
+}
+/**
+ {a=3, b=3, c=3, d=3, e=2}
+ */
