61f7f65166e341339f95d6c7f74614cfc256acbd
tron01 <69385258+tron01@users.noreply.github.com>
2024-11-27 21:26:52 +0530

    Scanner class

---
+++ myjava.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class myjava {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        System.err.println("Enter the num:");
+        int n = sc.nextInt();
+        System.err.println("num=" + n);
+    }
+}
